91b3953b307c8f453b5dabdf81d08276a445b5e6
ppding2025 <ppding2002@hotmail.com>
2026-06-26 20:49:15 +0100

    Add: is-anagram - submission-11

---
+++ Data Structures & Algorithms/is-anagram/submission-11.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    bool isAnagram(string s, string t) {
+        unordered_map<char,int> m1;
+        unordered_map<char,int> m2;
+        
+        
+        for (char c:s){
+           
+           m1[c]++;
+
+        }
+        for (char c:t){
+           
+           
+           m2[c]++;
+
+        }
+
+        return m1==m2;
+
+    }
+};
