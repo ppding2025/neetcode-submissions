16295de76093eba5a7d732348723b5447a8e49f1
ppding2025 <ppding2002@hotmail.com>
2026-07-07 18:38:24 +0100

    Add: search-2d-matrix - submission-13

---
+++ Data Structures & Algorithms/search-2d-matrix/submission-13.cpp
@@ -0,0 +1,61 @@
+class Solution {
+public:
+    bool searchMatrix(vector<vector<int>>& matrix, int target) {
+
+       
+       int low=0;
+       int high=matrix.size()-1;
+       int n=matrix[0].size()-1;
+       int l=0;
+       int r=n;
+       int mid;
+       while(low<=high){
+            mid= (low+high)/2;
+            if (matrix[mid][0]==target||matrix[mid][n]==target){
+
+                return true;
+            }
+            if (matrix[mid][0]<target){
+                if (target<matrix[mid][n]){
+                break;
+                }
+                low=mid+1;
+            }
+            else{
+                high=mid-1;
+            }
+            
+
+       }
+
+       while(l<r){
+
+        int m=(l+r)/2;
+        
+        if (matrix[mid][m]==target){
+
+            return true;
+        }
+        if (target < matrix[mid][m]){
+
+           r=m-1;
+
+        }
+        else{
+           
+           l=m+1;
+
+
+        }
+
+
+
+       }
+
+
+       return false;
+
+       
+
+    }
+};
