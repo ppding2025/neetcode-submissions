993be46ac1b7c923ad4c6e7a0ea214301058967e
ppding2025 <ppding2002@hotmail.com>
2026-07-08 19:21:42 +0100

    Add: time-based-key-value-store - submission-9

---
+++ Data Structures & Algorithms/time-based-key-value-store/submission-9.cpp
@@ -0,0 +1,51 @@
+class TimeMap {
+
+private:
+  unordered_map<string,vector<pair<string,int>>> mp;
+
+public:
+    TimeMap() {
+        
+    }
+    
+    void set(string key, string value, int timestamp) {
+        mp[key].push_back({value,timestamp});
+
+    }
+    
+    string get(string key, int timestamp) {
+        
+             
+            if (mp.find(key)==mp.end()){
+
+                 return "";
+            
+            }
+
+            
+            string ans;
+            for (auto it : mp[key]) {
+            int time = it.second;
+            string value = it.first;
+
+            if (time <= timestamp) {
+                ans = value;
+            } 
+            else {
+                break;
+            }
+            }
+
+            return ans;
+
+
+            } 
+        };
+        
+    
+
+  
+  
+
+
+
