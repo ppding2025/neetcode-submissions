083c7064549e9ac5ab8bdd3ef11dfca0bd877133
ppding2025 <ppding2002@hotmail.com>
2026-07-11 23:35:59 +0100

    Add: anagram-groups - submission-15

---
+++ Data Structures & Algorithms/anagram-groups/submission-15.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    vector<vector<string>> groupAnagrams(vector<string>& strs) {
+        unordered_map<string,vector<string>> mp;
+        
+        vector<vector<string>> ans;
+        for (int i=0; i<strs.size(); ++i) {
+            string key=strs[i];
+            sort(key.begin(),key.end());
+            
+            mp[key].push_back(strs[i]);
+            
+
+        }
+        for (auto it: mp){
+              ans.push_back(it.second);
+
+
+
+        }
+        return ans;
+    }
+};
