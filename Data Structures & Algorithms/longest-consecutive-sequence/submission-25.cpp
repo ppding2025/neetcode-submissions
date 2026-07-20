f543b27eacdefb05c4aa4b51583b4d5c6d9f5c2e
ppding2025 <ppding2002@hotmail.com>
2026-07-20 23:13:15 +0100

    Add: longest-consecutive-sequence - submission-25

---
+++ Data Structures & Algorithms/longest-consecutive-sequence/submission-25.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    int longestConsecutive(vector<int>& nums) {
+        unordered_set<int> pool;
+        int maxl=0;
+        for (int n:nums){
+            pool.insert(n);
+        }
+        for (int v: pool){
+                 int length=1;
+                 if (pool.count(v-1)) continue;
+                 while(pool.count(v+length)){
+                        
+                           length++;
+                           
+                 }
+                 maxl=max(maxl,length);
+
+        }
+        return maxl;
+    }
+};
