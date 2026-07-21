f7f4934a382ebb41a90bdefd945514b93a07b8fa
ppding2025 <ppding2002@hotmail.com>
2026-07-21 12:46:03 +0100

    Add: is-palindrome - submission-18

---
+++ Data Structures & Algorithms/is-palindrome/submission-18.cpp
@@ -0,0 +1,32 @@
+class Solution {
+public:
+    bool isPalindrome(string s) {
+        int l=0;
+        int r=s.size()-1;
+        while (l<=r){
+            
+            
+            
+            
+            while (l<s.size()&&!isalnum(s[l])){
+ 
+                l++;
+
+            }
+            while  (r>=0&&!isalnum(s[r])){
+
+               r--;
+            }
+           
+              if (tolower(s[l])!=tolower(s[r])){
+                 return false;
+            }
+              l++;
+              r--;
+
+
+           }
+
+        return true;
+    }
+};
