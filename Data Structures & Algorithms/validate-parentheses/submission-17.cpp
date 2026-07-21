ed820aaa4ad5f85ea2ff5d0d966db5aa2a14a9d0
ppding2025 <ppding2002@hotmail.com>
2026-07-21 17:15:56 +0100

    Add: validate-parentheses - submission-17

---
+++ Data Structures & Algorithms/validate-parentheses/submission-17.cpp
@@ -0,0 +1,36 @@
+class Solution {
+public:
+    bool isValid(string s) {
+      
+      stack<char> st;
+      for (int i=0;i<s.size(); ++i){
+         if (s[i]=='('||s[i]=='['||s[i]=='{'){
+             st.push(s[i]); 
+
+         }
+         else{
+             if (st.empty()){
+
+               return false;
+             }
+             char c=st.top();
+             if (abs(s[i]-c)>2){
+
+               return false;
+             } 
+             
+             st.pop();
+
+
+         }
+
+
+      }  
+      if (st.size()==0){
+      return true;
+      }
+
+      return false;
+
+    }
+};
