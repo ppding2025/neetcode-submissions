a2eb66ac2a0b31a0632a33ae4ebdcb833e38bb99
ppding2025 <ppding2002@hotmail.com>
2026-07-23 22:07:23 +0100

    Add: find-minimum-in-rotated-sorted-array - submission-32

---
+++ Data Structures & Algorithms/find-minimum-in-rotated-sorted-array/submission-32.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int findMin(vector<int> &nums) {
+        
+       int minele=nums[0];
+
+       for (int i=0; i<nums.size(); ++i){
+
+            minele=min(minele,nums[i]);
+
+       }
+        return minele;
+    }
+    
+};
