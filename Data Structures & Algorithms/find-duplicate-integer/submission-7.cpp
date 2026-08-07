af7c61513bdffbec56b52d61cf2c6157e85e19f8
ppding2025 <ppding2002@hotmail.com>
2026-08-07 11:57:56 +0100

    Add: find-duplicate-integer - submission-7

---
+++ Data Structures & Algorithms/find-duplicate-integer/submission-7.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    int findDuplicate(vector<int>& nums) {
+        for (int i=0; i<nums.size();++i){
+
+           for (int j=i+1; j<nums.size();++j){
+
+              if (nums[j]==nums[i]){
+
+                return nums[j];
+              }
+
+
+           }
+
+
+        }
+
+    }
+};
