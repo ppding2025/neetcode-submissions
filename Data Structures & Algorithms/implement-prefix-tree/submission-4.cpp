71b301bfd218d28f07ff722d36c0974ba02eb2ab
ppding2025 <ppding2002@hotmail.com>
2026-08-12 16:00:31 +0100

    Add: implement-prefix-tree - submission-4

---
+++ Data Structures & Algorithms/implement-prefix-tree/submission-4.cpp
@@ -0,0 +1,81 @@
+
+
+
+
+class PrefixTree {
+public:
+    
+    struct TrieNode{
+           
+           unordered_map<char,TrieNode*> children;
+
+           bool isEnd;
+           
+           TrieNode(){
+               
+               isEnd=false; 
+
+           }
+
+
+
+    };
+
+    TrieNode* root;
+    
+    PrefixTree() {
+         root=new TrieNode();           
+    }
+    
+    void insert(string word) {
+        
+        TrieNode* curr=root;
+        for (char c:word){
+            if (!curr->children.count(c)){
+               
+               curr->children[c]=new TrieNode();
+               
+            }
+            curr=curr->children[c];
+        }
+        curr->isEnd=true;
+
+    }
+    
+    bool search(string word) {
+        TrieNode* curr=root;
+        for (char c:word){
+
+            if (!curr->children.count(c)){
+
+                return false;
+            }
+            curr=curr->children[c];
+
+        }
+        if (curr->isEnd){
+            return true;
+        }
+        else{
+
+            return false;
+        }
+        
+    }
+    
+    bool startsWith(string prefix) {
+        TrieNode* curr=root;
+        for (char c:prefix){
+
+            if (!curr->children.count(c)){
+
+                return false;
+            }
+            curr=curr->children[c];
+
+        }
+        return true;
+
+
+    }
+};
