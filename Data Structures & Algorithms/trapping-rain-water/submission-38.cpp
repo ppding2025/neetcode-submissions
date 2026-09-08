8b403321c756723c36ee72203ea18489ace1b648
ppding2025 <ppding2002@hotmail.com>
2026-09-08 11:24:48 +0100

    Add: trapping-rain-water - submission-38

---
+++ Data Structures & Algorithms/trapping-rain-water/submission-38.cpp
@@ -0,0 +1,43 @@
+class Solution {
+public:
+    int trap(vector<int>& height) {
+        int ans=0;
+        int l=0;
+
+        int r=height.size()-1;
+        int lmax=height[l];
+        int rmax=height[r]; 
+
+        while (l<r){
+              if (lmax<=rmax){
+                  l++;
+                  lmax=max(lmax,height[l]);
+                  ans+=lmax-height[l];
+
+
+
+              }
+              else{
+                  r--;
+                  rmax=max(rmax,height[r]);
+                  ans+=rmax-height[r];
+
+              }
+
+
+
+        }
+
+            
+    return ans;
+
+
+        
+
+            
+
+
+        
+
+    }
+};
